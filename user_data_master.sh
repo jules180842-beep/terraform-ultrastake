1543bc39958c8f1212226216ebf133ea594492a9
jules180842-beep <jules180842@gmail.com>
2026-07-04 00:58:05 -0600

    Update user_data_master.sh

--- user_data_master.sh
+++ user_data_master.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-apt update -y
-apt install -y python3 python3-pip git
-
-cd /home/ubuntu
-git clone https://github.com/YOUR_ORG/ultrastake-core.git
-cd ultrastake-core
-
-pip3 install -r requirements.txt
-
-export NODE_ROLE=master
-export PORT=8000
-
-nohup python3 -m api.server > master.log 2>&1 &
+python3 -m uvicorn api.server:app --host 0.0.0.0 --port 8000
