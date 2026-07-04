0382f1da05e94e58bef2b8b36f03ee8d588a95ab
jules180842-beep <jules180842@gmail.com>
2026-07-04 00:44:23 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "ultrastake_sg" {
+  name        = "ultrastake-sg"
+  description = "UltraStake EC2 cluster security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8765
+    to_port     = 8765
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
